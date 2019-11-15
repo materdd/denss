a43cc00190fe7b2df68142df66c4192d2e38390b
materdd <shinichiro.murakami@instalimb.com>
2019-11-16 00:10:22 +0900

    modify a lot

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+python bin/denss.fit_data.py -f 6lyz.dat
+python bin/denss.py -f 6lyz_fit.dat --plot_on --steps 1000 -d 50.0
 python bin/denss.py -f 6lyz.out --plot_on --steps 1000
+python bin/denss.all.py -f 6lyz.out -j 4 --nmaps 2
